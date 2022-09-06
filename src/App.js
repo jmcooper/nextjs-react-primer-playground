0dbbe12de3bfce4c578d1c99f5f86af3fbc3624f
Jim Cooper <jim@cooperplanet.com>
2022-09-06 14:36:13 -0600

    Update styling

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Welcome to the</h2>
-      <h1>Next.js Playbook: React Primer Playground</h1>
-      <h3>Start editing App.js to get started!</h3>
+    <div className="container">
+      <h1 className="title">
+        Welcome to the{' '}
+        <span className="playground">React Primer Playground!</span>
+      </h1>
+
+      <p className="getStarted">
+        Get started by editing{' '}
+        <code className="code">/src/App.js</code>
+      </p>
+
+      <p className="description">
+        This playground is for use with the
+        Pluralsight React with Next.js Playbook Course
+      </p>
     </div>
   );
 }
